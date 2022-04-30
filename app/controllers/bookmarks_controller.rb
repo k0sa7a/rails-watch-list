2de0f465cf80eecf1f9a7f0c90fcf015ee9a1056
Stefan Filev <stefan.s.filev@gmail.com>
2022-04-30 19:04:24 +0100

    Updated the Show for Lists to include more comment of bookmark and a delete link for bookmark. Added Bookmark Controller destroy action

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,32 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(params_bookmark)
+    @bookmark.list = @list
+    # Alternative way I was using without strong params for movie_id
+    # @movie = Movie.find(params[:bookmark][:movie_id])
+    # @bookmark.movie = @movie
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def params_bookmark
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
